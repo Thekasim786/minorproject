7bec6cadb3373c95660aafb9057a3a0707f95cc7
Thekasim786 <mohammadkassimmalik1@gmail.com>
2025-04-18 13:04:51 +0530

    video page and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // ✅ this line was missing
 import NavbarLogout from './components/NavbarLogout';
-// import Homepage from './components/omepage';
+
 import SignupPage from './components/SignupPage'
-// import Login from './Login'
-import Homepage from './components/homepage';
+import LandingPage from './components/LandingPage';
+
 import Login from './components/login';
+import HomePage from './components/Homepage';
+import VideoPlayerPage from './components/VideoPlayerPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/video" element={<VideoPlayerPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+import NavbarLogout from "./NavbarLogout";
+export default function LandingPage() {
+    return (
+      <>
+        <NavbarLogout />
+        <h1>Hello</h1>
+      </>
+    );
+  }
